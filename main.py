fcae5fb7d06f262dbd6193052a3a5a1b2d92a2bb
LotusHime <99133037+LotusHime@users.noreply.github.com>
2023-11-21 13:13:22 +0500

    breadth-first search

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-graph = {}
+from collections import deque
+
+graph = dict()
 
 graph["you"] = ["alice", "bob", "claire"]
+graph["alice"] = ['rustam']
+
 
 graph["bob"] = ["anuj", "peggy"]
 
@@ -14,3 +18,31 @@ graph["thom"] = []
 graph["jonny"] = []
 
 print(graph)
+print(graph["you"])
+
+search_queue = deque()
+search_queue += graph['you']
+
+
+print(search_queue)
+
+
+def person_is_seller(name): # функция проверяет заканчивается ли имя на определенную букву
+    return name[-1] == 'm'
+
+def search(name):
+    search_queue = deque()
+    search_queue += graph[name]
+    searched = []
+    while search_queue:
+        person = search_queue.popleft()
+        if not person in searched:
+            if person_is_seller(person):
+                print(
+                    f'\n{person.title()} is a mango seller!')  # Условие выполняется верно-> это продавец, выводит на консоль
+            else:
+                search_queue += graph[person]
+                searched.append(person)
+
+search('you')
+
